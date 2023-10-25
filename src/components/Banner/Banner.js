fc224c31cc6997e6ad750ae9efec5fde79d9d645
Pedro Henrique <pedrotxc@gmail.com>
2023-10-25 13:53:40 -0300

    Adicionando banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header class="banner">
+            <img src="/imagens/Banner.png"></img>
+        </header>
+    )
+}
+
+export default Banner
