17025ae7192f7b1e2f8a564900e986f657f59412
Pedro Henrique <pedrotxc@gmail.com>
2024-07-08 23:06:26 -0300

    Adicionando lista Suspensa

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,10 +3,16 @@ import './CampoTexto.css';
 const CampoTexto = (props) => {
     const placeholderModificada = `${props.placeholder}...`
 
+    let valor = 'Pedro Henrique';
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={placeholderModificada}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,34 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const times = [
+        'Proramação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Desing',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome " />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo " />
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem " />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome " />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo " />
+                <CampoTexto obrigatorio={true} label="Imagem" placeholder="Digite o endereço da imagem " />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>Criar Card</Botao>
             </form>
         </section>
     )
